9556bcae1ee80a739e6af54ebb30d1c1a0aa85f6
unknown <wjse0213@naver.com>
2022-07-17 19:24:38 +0900

    모델구성:01.11

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from distutils.log import debug
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+# mysql과 연결 
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:root@db/main' # mysql에 유저네임 :페스워드 @호스트(docker서비스명)/db이름
+CORS(app)
+# db 모델 구성 알키미 패키지를 이용해 모델처럼 구성한다 
+db=SQLAlchemy(app)
+
+class Shop():
+    id=db.Column(db.Integer,primary_key=True,autoincrement=False) # 장고의 db와 싱크를 맞춰야함 
+    shop_name=db.Column(db.String(200))
+    shop_address=db.Column(db.String(200))
+
+class Order(): # boss 데이터베이스는 order date와 deliverFinish를 가지고 있을 필요가 없음
+    id=db.Column(db.Integer,primary_key=True,autoincrement=False) # 장고의 db와 싱크를 맞춰야함 
+    shop=db.Column(db.Integer)
+    address=db.Column(db.String(200))
+
 
 @app.route('/')
 def index():
